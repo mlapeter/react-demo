f3273581077dc130f1bf0b15fd3e270a6945a99e
Mike LaPeter <mlapeter@gmail.com>
2022-01-06 13:12:31 -0700

    Initial commit

---
+++ src/example.js
@@ -0,0 +1,13 @@
+
+export default function Button() {
+  return (
+    <button
+      type="button"
+      className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      onClick={() => console.log('clicked')}
+    >
+      click me
+    </button>
+  );
+}
+
